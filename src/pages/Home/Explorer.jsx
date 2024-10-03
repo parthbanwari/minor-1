f4f8f61b68a9940800431a07ec09b899016198b0
r-ajvardhan <r-ajvardhan@users.noreply.github.com>
2024-10-03 19:55:24 +0530

    update

--- src/pages/Home/Explorer.jsx
+++ src/pages/Home/Explorer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const RightComponent = () => {
+const Explorer = () => {
     return (
         <div className='p-5 bg-night-blue text-light-purple '>
             {/* Header Section */}
@@ -15,14 +15,12 @@ const RightComponent = () => {
             {/* Folders Container */}
             <div className=' overflow-auto h-[625px] hide-scrollbar'>
                 {/* folder 1 */}
-                <div className='mt-5 bg-dark-gray px-4 pb-2 rounded'>
+                <div className='mt-2 bg-dark-gray px-4 pb-2 rounded'>
                     <div className='flex justify-between border-b border-charcoal py-2'>
                         {/* Left side of the folder header */}
                         <div className='flex gap-5 items-center'>
                             <i className='fas fa-folder text-ice-blue'></i>
-                            <span className='text-pale-blue'>
-                                Folder 1
-                            </span>
+                            <span className='text-pale-blue'>Folder 1</span>
                         </div>
 
                         {/* Right side of the folder header */}
@@ -37,7 +35,7 @@ const RightComponent = () => {
                     </div>
 
                     {/* Cards Container */}
-                    <div className=' mt-5 grid grid-cols-3 gap-2'>
+                    <div className=' mt-2 grid grid-cols-3 gap-2'>
                         {/* Sample Compact Card 1 */}
                         <div className='bg-dusk p-3 rounded flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow'>
                             <div>
@@ -97,30 +95,11 @@ const RightComponent = () => {
                                 </button>
                             </div>
                         </div>
-                        <div className='bg-dusk p-3 rounded flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow'>
-                            <div>
-                                <h2 className='text-pale-blue text-sm font-semibold'>
-                                    File 3
-                                </h2>
-                                <p className='text-light-purple-alt text-xs'>
-                                    Brief description of File 3.
-                                </p>
-                            </div>
-                            <div className='flex justify-end mt-2'>
-                                <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1 hover:bg-opacity-75'>
-                                    <i className='fas fa-edit text-xs mr-1'></i>
-                                </button>
-                                <button className='flex items-center bg-red text-white px-2 py-1 rounded hover:bg-opacity-75'>
-                                    <i className='fas fa-trash text-xs mr-1'></i>
-                                </button>
-                            </div>
-                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     )
 }
 
-export default RightComponent
+export default Explorer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import RightComponent from './RightComponent'
+import Explorer from './Explorer'
 import LeftComponent from './LeftComponent'
 
 const Home = () => {
     return (
         <div className='home-container grid grid-cols-[3fr_4fr] h-screen bg-night-blue'>
             <LeftComponent />
-            <RightComponent />
+            <Explorer />
         </div>
     )
 }
