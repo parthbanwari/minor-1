7b1ed6029c9de4b6fd74348bc02aa50676dbbe48
r-ajvardhan <r-ajvardhan@users.noreply.github.com>
2024-10-03 13:48:00 +0530

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,7 @@ import LeftComponent from './LeftComponent'
 const Home = () => {
     return (
         <div className='home-container grid grid-cols-[3fr_4fr] h-screen bg-night-blue'>
-            {/* Left Container */}
             <LeftComponent />
-            {/* Right Component */}
             <RightComponent />
         </div>
     )
--- src/pages/Home/RightComponent.jsx
+++ src/pages/Home/RightComponent.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-export const RightComponent = () => {
+const RightComponent = () => {
     return (
         <div className='p-5 bg-night-blue text-light-purple'>
             {/* Header Section */}
             <div className='flex justify-between py-2 border-b-2 border-charcoal'>
-                <h1 className='text-light-purple'>My Workspace</h1>
-                <button className='flex justify-center items-center bg-coral text-night-blue px-3 py-2 rounded'>
+                <h1 className='text-light-purple'>Explorer</h1>
+                <button className='flex justify-center items-center bg-coral text-night-blue px-3 py-2 rounded hover:bg-opacity-75'>
                     <i className='fas fa-folder-plus mr-2'></i>
                     <span>New Folder</span>
                 </button>
             </div>
 
             {/* Folder Container */}
-            <div className='mt-5 bg-dark-gray p-4 rounded'>
+            <div className='mt-5 bg-dark-gray px-4 rounded'>
                 <div className='flex justify-between border-b border-charcoal py-5'>
                     {/* Left side of the folder header */}
                     <div className='flex gap-5 items-center'>
@@ -25,27 +25,31 @@ export const RightComponent = () => {
                     <div className='flex gap-5 items-center'>
                         <i className='fas fa-trash text-red cursor-pointer'></i>
                         <i className='fas fa-edit text-light-purple-alt cursor-pointer'></i>
-                        <button className='bg-hot-pink text-night-blue px-3 py-2 rounded flex items-center'>
-                            <i className='fas fa-plus mr-2'></i>
-                            <span>New Workspace</span>
+                        <button className='bg-ice-blue text-night-blue px-3 py-2 rounded flex items-center hover:bg-opacity-75'>
+                            <i className='fas fa-file mr-2'></i>
+                            <span>File</span>
                         </button>
                     </div>
                 </div>
             </div>
 
             {/* Cards Container */}
-            <div className='cards-container mt-5 grid grid-cols-2 gap-4'>
+            <div className='cards-container mt-5 grid grid-cols-3 gap-2'>
                 {/* Sample Compact Card 1 */}
                 <div className='bg-dark-gray p-3 rounded flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow'>
                     <div>
-                        <h2 className='text-pale-blue text-sm font-semibold'>File 1</h2>
-                        <p className='text-light-purple-alt text-xs'>Brief description of File 1.</p>
+                        <h2 className='text-pale-blue text-sm font-semibold'>
+                            File 1
+                        </h2>
+                        <p className='text-light-purple-alt text-xs'>
+                            Brief description of File 1.
+                        </p>
                     </div>
                     <div className='flex justify-end mt-2'>
-                        <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1'>
+                        <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1 hover:bg-opacity-75'>
                             <i className='fas fa-edit text-xs mr-1'></i>
                         </button>
-                        <button className='flex items-center bg-red text-white px-2 py-1 rounded'>
+                        <button className='flex items-center bg-red text-white px-2 py-1 rounded hover:bg-opacity-75'>
                             <i className='fas fa-trash text-xs mr-1'></i>
                         </button>
                     </div>
@@ -54,14 +58,38 @@ export const RightComponent = () => {
                 {/* Sample Compact Card 2 */}
                 <div className='bg-dark-gray p-3 rounded flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow'>
                     <div>
-                        <h2 className='text-pale-blue text-sm font-semibold'>File 2</h2>
-                        <p className='text-light-purple-alt text-xs'>Brief description of File 2.</p>
+                        <h2 className='text-pale-blue text-sm font-semibold'>
+                            File 2
+                        </h2>
+                        <p className='text-light-purple-alt text-xs'>
+                            Brief description of File 2.
+                        </p>
                     </div>
                     <div className='flex justify-end mt-2'>
-                        <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1'>
+                        <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1 hover:bg-opacity-75'>
                             <i className='fas fa-edit text-xs mr-1'></i>
                         </button>
-                        <button className='flex items-center bg-red text-white px-2 py-1 rounded'>
+                        <button className='flex items-center bg-red text-white px-2 py-1 rounded hover:bg-opacity-75'>
+                            <i className='fas fa-trash text-xs mr-1'></i>
+                        </button>
+                    </div>
+                </div>
+
+                {/* Sample Compact Card 3 */}
+                <div className='bg-dark-gray p-3 rounded flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow'>
+                    <div>
+                        <h2 className='text-pale-blue text-sm font-semibold'>
+                            File 3
+                        </h2>
+                        <p className='text-light-purple-alt text-xs'>
+                            Brief description of File 3.
+                        </p>
+                    </div>
+                    <div className='flex justify-end mt-2'>
+                        <button className='flex items-center bg-light-purple-alt text-night-blue px-2 py-1 rounded mr-1 hover:bg-opacity-75'>
+                            <i className='fas fa-edit text-xs mr-1'></i>
+                        </button>
+                        <button className='flex items-center bg-red text-white px-2 py-1 rounded hover:bg-opacity-75'>
                             <i className='fas fa-trash text-xs mr-1'></i>
                         </button>
                     </div>
@@ -70,3 +98,5 @@ export const RightComponent = () => {
         </div>
     )
 }
+
+export default RightComponent
